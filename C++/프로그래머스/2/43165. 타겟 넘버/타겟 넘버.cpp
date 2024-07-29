fd8c73b56d3a431977c99f780cf5d553d3feba4c
yunni0315 <117873400+yunni0315@users.noreply.github.com>
2024-07-29 16:34:51 +0900

    [level 2] Title: 타겟 넘버, Time: 3.10 ms, Memory: 4.15 MB -BaekjoonHub

--- C++/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ C++/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -2,16 +2,19 @@
 
 using namespace std;
 int answer = 0;
-void DFS(int N, int sum, vector<int> &numbers, int target){
-    if(N==numbers.size()){
-        if(sum==target) answer++;
+
+void DFS(int N, int sum, int target, vector<int> &numbers){
+    if(N == numbers.size()){
+        if(sum == target)
+            answer++;
         return;
     }
-    DFS(N+1, sum+numbers[N], numbers, target);
-    DFS(N+1, sum-numbers[N], numbers, target);
+    
+    DFS(N+1, sum+numbers[N], target, numbers);
+    DFS(N+1, sum-numbers[N], target, numbers);
 }
 
 int solution(vector<int> numbers, int target) {
-     DFS(0, 0, numbers, target);
+    DFS(0, 0, target, numbers);
     return answer;
 }
