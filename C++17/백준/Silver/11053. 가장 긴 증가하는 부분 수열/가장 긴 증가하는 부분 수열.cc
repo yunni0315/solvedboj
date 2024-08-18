f66df37af25c91b7eff8daa0c7965062d83e4e84
yunni0315 <117873400+yunni0315@users.noreply.github.com>
2024-08-18 16:30:55 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int A[1001];int V[1001];
+int main() {
+	int N, cnt = 0; int Ai; 
+	
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		int max = 0;
+		scanf("%d", &Ai);
+		A[i] = Ai;
+		if (i == 0) V[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (A[j] < Ai) {
+				if (max < V[j])
+					max = V[j];
+			}
+		}
+		V[i] = max + 1;
+	}
+	int max = -1;
+	for (int i = 0; i < N; i++) {
+		if (max < V[i]) {
+			max = V[i];
+		}
+	}
+
+	printf("%d", max);
+	return 0;
+}
