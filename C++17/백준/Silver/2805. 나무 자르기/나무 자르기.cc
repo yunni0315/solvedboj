0edb29b36a205ab4c33ff9b6922c0c5d53ce5f8e
yunni0315 <117873400+yunni0315@users.noreply.github.com>
2024-10-13 18:22:07 +0900

    [Silver II] Title: 나무 자르기, Time: 160 ms, Memory: 5804 KB -BaekjoonHub

--- C++17/백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ C++17/백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -3,46 +3,43 @@
 #include <algorithm>
 using namespace std;
 
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
-    int trees[1000001];
-    long long int N, M, max_height = 0;
-    cin >> N >> M;
+	int trees[1000001];
+	long long int N, M, max = 0;
+	cin >> N >> M;
 
-    for (int i = 0; i < N; i++) {
-        cin >> trees[i];
-    }
-    sort(trees, trees + N);
+	for (int i = 0; i < N; i++) {
+		cin >> trees[i];
+	}
+	sort(trees, trees + N);
 
-    long long low = 0;
-    long long high = trees[N - 1];
+	long long low = 0;
+	long long high = trees[N - 1];
 
-    while (high >= low) {
-        long long mid = (low + high) / 2;
-        long long sum = 0;
+	while (high >= low) {
+		long long mid = (low + high) / 2;
+		long long sum = 0;
 
-        // 잘라서 얻은 나무 길이 합 계산
-        for (int i = 0; i < N; i++) {
-            if (trees[i] > mid) {
-                sum += trees[i] - mid;
-            }
-        }
+		for (int i = 0; i < N; i++) {
+			if (trees[i] > mid) {
+				sum += trees[i] - mid;
+			}
+		}
 
-        // 합이 M 이상이면 톱 높이를 높여본다.
-        if (sum >= M) {
-            max_height = mid;  // 일단 현재 높이 저장
-            low = mid + 1;
-        }
-        // 합이 M 미만이면 톱 높이를 낮춘다.
-        else {
-            high = mid - 1;
-        }
-    }
+		if (sum >= M) {
+			low = mid + 1;
+			max = mid;
+		}
+		else if (sum < M) {
+			high = mid - 1;
+		}
+	}
+	cout << max;
 
-    cout << max_height;
-
-    return 0;
+	return 0;
 }
