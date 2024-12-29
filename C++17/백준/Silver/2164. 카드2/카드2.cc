48a33121a5303e779db1faaa9e835b8b9212c85f
yunni0315 <117873400+yunni0315@users.noreply.github.com>
2024-12-29 13:49:43 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

--- C++17/백준/Silver/2164. 카드2/카드2.cc
+++ C++17/백준/Silver/2164. 카드2/카드2.cc
@@ -3,25 +3,26 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
-    int N;
-    cin >> N;
+	int N;
+	queue<int> Q;
 
-    queue<int> Q;
+	cin >> N;
 
-    for (int i = 1; i <= N; i++) {
-        Q.push(i);
-    }
+	for (int i = 1; i <= N; i++) {
+		Q.push(i);
+	}
 
-    while (Q.size() > 1) { 
-        Q.pop();           
-        Q.push(Q.front()); 
-        Q.pop();           
-    }
+	while (Q.size() != 1) {
+		Q.pop();
+		Q.push(Q.front());
+		Q.pop();
+	}
 
-    cout << Q.front();      
-    return 0;
+	cout << Q.back();
+
+	return 0;
 }
