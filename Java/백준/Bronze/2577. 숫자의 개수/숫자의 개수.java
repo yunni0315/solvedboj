c04f441675f1bf15a39ea5c68d73ab6038194e2a
yunni0315 <117873400+yunni0315@users.noreply.github.com>
2025-08-10 11:13:54 +0900

    [Bronze II] Title: 숫자의 개수, Time: 168 ms, Memory: 17488 KB -BaekjoonHub

--- Java/백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ Java/백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -15,36 +15,7 @@ public class Main {
 
         for (int i = 0; i < Dstr.length(); i++) {
             int num = Dstr.charAt(i) - '0';
-            if (num == 0){
-                numbers[num]++;
-            }
-            else if (num == 1){
-                numbers[num]++;
-            }
-            else if (num == 2){
-                numbers[num]++;
-            }
-            else if (num == 3){
-                numbers[num]++;
-            }
-            else if (num == 4){
-                numbers[num]++;
-            }
-            else if (num == 5){
-                numbers[num]++;
-            }
-            else if (num == 6){
-                numbers[num]++;
-            }
-            else if (num == 7){
-                numbers[num]++;
-            }
-            else if (num == 8){
-                numbers[num]++;
-            }
-            else {
-                numbers[num]++;
-            }
+            numbers[num]++;
         }
 
         for (int i = 0; i < 10; i++) {
